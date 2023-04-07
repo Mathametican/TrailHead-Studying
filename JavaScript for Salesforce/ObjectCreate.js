bc9a225096b77aa4eb33ef99b7dc0e2e0ee1f499
Kübra Sert <elmastas571@gmail.com>
2023-04-07 08:53:27 +0300

    My New Working

---
+++ JavaScript for Salesforce/ObjectCreate.js
@@ -0,0 +1,25 @@
+const bike ={
+    frontGearIndex:0,
+    rearGearIndex:0,
+    transmission:{
+        frontGearTeeth:[30,45],
+        rearGearTheeth:[11,13,15,17,19,21,24,28,32,36]
+    },
+    calculateGearRatio:function(){
+        let front=this.transmission.frontGearTeeth[this.frontGearIndex],
+        rear =this.transmission.rearGearTheeth[this.rearGearIndex];
+
+        return (front/ratio);
+    },
+
+    changeGear: function(frontOrRear,newValue){
+        if(frontOrRear==='front'){
+            this.frontGearIndex=newValue;
+        } else {
+            this.rearGearIndex=newValue;
+        }
+    }
+};
+bike.frontGearIndex,
+bike.transmission.frontGearTeeth,
+bike.calculateGearRatio(),
