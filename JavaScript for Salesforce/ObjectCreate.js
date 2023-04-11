69b6770e6b9a76e2ee9e9c0f73e335354c998cb6
Kübra Sert <elmastas571@gmail.com>
2023-04-11 12:24:38 +0300

    new works

--- JavaScript for Salesforce/ObjectCreate.js
+++ JavaScript for Salesforce/ObjectCreate.js
@@ -1,4 +1,41 @@
-const bike ={
+const bike1={
+gears:10,
+currentGear:3,
+changeGear: function(direction,changeBy){
+    if(direction==='up'){
+        this.currentGear += changeBy;
+    }else {
+        this.currentGear -=changeBy;
+    }
+}
+}
+console.log(bike1.gears);
+console.log(bike1.currentGear);
+bike1.changeGear('up',1);
+console.log(bike1.currentGear);
+bike1.changeGear('up',1);
+console.log(bike1.currentGear);
+
+//Create object with Constructor
+
+function Bike(gears, startGear){
+    this.gears=gears;
+    this.currentGear=startGear;
+}
+Bike.prototype.changeGear = function(direction,changeBy){
+    if(direction ==='up'){
+        this.currentGear += changeBy;
+    }else {
+        this.currentGear -= changeBy;
+    }
+}
+const bike = new Bike(10,3);
+console.log(bike.gears);
+console.log(bike.currentGear);
+bike.changeGear('up',1);
+console.log(bike.currentGear);
+
+const bike2={
     frontGearIndex:0,
     rearGearIndex:0,
     transmission:{
